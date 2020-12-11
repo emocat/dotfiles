a876ae0308bc5cc49aed887304d60cb59195e990
emocat <839194377@qq.com>
2020-12-11 21:25:50 +0800

    Feat: improve template again

--- templates/=template=hack.py
+++ templates/=template=hack.py
@@ -5,25 +5,24 @@
 # Distributed under terms of the %LICENSE% license.
 
 from pwn import *
+
 context.arch = 'amd64'
-context.log_level = "debug"
+context.log_level = 'debug'
 context.terminal = ['tmux', 'split', '-h']
 
 RHOST = ""
 RPORT = 0 
 
 binary = ("./pwn")
-elf = ELF([binary])
+elf = ELF(binary)
 libc = elf.libc
 
-if not args['REMOTE']:
-    r = process(binary)
-else:
+if args['REMOTE']:
     r = remote(RHOST, RPORT)
-
-def debug():
-    if len(sys.argv) > 1 and sys.argv[1] == 'd':
-        gdb.attach(r, "")
+elif args['GDB']:
+    r = gdb.debug(binary)
+else:
+    r = process([binary])
 
 %HERE%
 
